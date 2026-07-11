334aa6dbcbbf4b0a17aff7a045a23dabcc3f77e9
semperthY <hamzter76@gmail.com>
2026-07-11 19:44:12 +0300

    Добавлено мобильное меню

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,12 @@ export default function Header(){
       <h1 className="text-2xl font-bold">
         🐘 СЫТЫЙ СЛОНЪ
       </h1>
-<button className="text-3xl md:hidden">
-  ☰
-</button>
+      <button
+      className="text-3xl md:hidden"
+      onClick={() => setMenuOpen(!menuOpen)}
+      >
+      ☰
+      </button>
       <nav className="hidden md:flex gap-6">
         <a href="#">Главная</a>
         <a href="#">Меню</a>
@@ -18,5 +21,15 @@ export default function Header(){
         <a href="#">Контакты</a>
       </nav>
     </header>
+</header>
+
+{menuOpen && (
+  <nav className="flex flex-col gap-4 px-6 pb-4 md:hidden">
+    <a href="#">Главная</a>
+    <a href="#">Меню</a>
+    <a href="#">О нас</a>
+    <a href="#">Контакты</a>
+  </nav>
+)}
   );
 }
