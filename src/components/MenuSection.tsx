da3a8302466b397474e6b2b79f9a3fa9726a0784
semperthY <hamzter76@gmail.com>
2026-07-11 20:17:51 +0300

    Краткое описание изменений

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -1,32 +1,29 @@
+import { menu } from "../data/menu";
 export default function MenuSection() {
   return (
     <section className="px-6 py-16 bg-neutral-900 text-white">
       <h2 className="text-4xl font-bold text-center mb-10">
         Наше меню
       </h2>
+ <div className="grid gap-6 md:grid-cols-3">
+       {menu.map((item) => (
+        <div
+        key={item.id}
+        className="rounded-2xl bg-neutral-800 p-6"
+        >
+       <h3 className="text-2xl font-semibold">
+       {item.name}
+       </h3>
 
-      <div className="grid gap-6 md:grid-cols-3">
+       <p className="mt-2 text-yellow-400 font-bold">
+       {item.price}
+       </p>
 
-        <div className="rounded-2xl bg-neutral-800 p-6">
-          <h3 className="text-2xl font-semibold">☕ Кофе</h3>
-          <p className="mt-3 text-neutral-300">
-            Эспрессо, американо, капучино, латте.
-          </p>
-        </div>
-
-        <div className="rounded-2xl bg-neutral-800 p-6">
-          <h3 className="text-2xl font-semibold">🥐 Завтраки</h3>
-          <p className="mt-3 text-neutral-300">
-            Омлеты, сырники, круассаны, каши.
-          </p>
-        </div>
-
-        <div className="rounded-2xl bg-neutral-800 p-6">
-          <h3 className="text-2xl font-semibold">🍰 Десерты</h3>
-          <p className="mt-3 text-neutral-300">
-            Чизкейк, тирамису, эклеры и торты.
-          </p>
-        </div>
+       <p className="mt-3 text-neutral-300">
+       {item.description}
+    </p>
+  </div>
+))}
 
       </div>
     </section>
