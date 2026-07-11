bcd64623d9f85fb28834a1c631696d2aa57654c7
semperthY <hamzter76@gmail.com>
2026-07-11 18:40:51 +0300

    v0.3 Добавлен Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-neutral-950 text-white flex flex-col items-center justify-center px-6">
-      <h1 className="text-5xl font-bold">🐘 СЫТЫЙ СЛОНЪ</h1>
+    <>
+      <Header />
 
-      <p className="mt-6 text-center max-w-md text-neutral-300">
-        Добро пожаловать в кафе «Сытый Слонъ».
-      </p>
-    </main>
+      <main className="min-h-screen bg-neutral-950 text-white flex flex-col items-center justify-center px-6">
+        <h2 className="text-5xl font-bold">
+          Добро пожаловать!
+        </h2>
+
+        <p className="mt-6 max-w-md text-center text-neutral-300">
+          Добро пожаловать в кафе «Сытый Слонъ».
+        </p>
+      </main>
+    </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between p-6 bg-neutral-900 text-white">
+      <h1 className="text-2xl font-bold">
+        🐘 СЫТЫЙ СЛОНЪ
+      </h1>
+
+      <nav className="hidden md:flex gap-6">
+        <a href="#">Главная</a>
+        <a href="#">Меню</a>
+        <a href="#">О нас</a>
+        <a href="#">Контакты</a>
+      </nav>
+    </header>
+  );
+}
