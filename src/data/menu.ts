696ba7d5a8f2fc4cc5860e06e195204bf20c752e
semperthY <hamzter76@gmail.com>
2026-07-19 09:09:08 +0300

    Fix menu types

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,20 +1,32 @@
-export const menu = [
+export type MenuItem = {
+  id: number;
+  category: string;
+  name: string;
+  price: string;
+  weight?: string;
+  description?: string;
+};
+
+export const menu: MenuItem[] = [
   {
     id: 1,
+    category: "Кофе",
     name: "Капучино",
-    price: 4.5,
-    description: "Классический кофе с молочной пенкой"
+    price: "4.50 €",
+    description: "Классический кофе с молочной пенкой",
   },
   {
     id: 2,
+    category: "Десерты",
     name: "Сырники",
-    price: 8.9,
-    description: "Домашние сырники со сметаной"
+    price: "8.90 €",
+    description: "Домашние сырники со сметаной",
   },
   {
     id: 3,
+    category: "Десерты",
     name: "Чизкейк",
-    price: 5.9,
-    description: "Нежный сливочный чизкейк"
-  }
+    price: "5.90 €",
+    description: "Нежный сливочный чизкейк",
+  },
 ];
