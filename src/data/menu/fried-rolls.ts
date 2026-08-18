2df14da86ff6316c754936bf9580cb4cc7343300
semperthY <hamzter76@gmail.com>
2026-08-18 16:01:24 +0300

    Fix fried rolls data shape

--- src/data/menu/fried-rolls.ts
+++ src/data/menu/fried-rolls.ts
@@ -1,34 +1,39 @@
 import type { MenuItem } from "./types";
 
 export const friedRolls: MenuItem[] = [
-  {
-    name: "Хаски",
-    description: "Лосось, огурец, сыр творожный, кляр, сухари панко, рис, нори",
-    price: 450,
-    amount: "8 шт.",
-  },
-  {
-    name: "Ролл с лососем",
-    description: "Лосось, кляр, сухари панко, рис, нори",
-    price: 350,
-    amount: "8 шт.",
-  },
-  {
-    name: "Ролл с курицей",
-    description: "Курица, кляр, сухари панко, рис, нори",
-    price: 350,
-    amount: "8 шт.",
-  },
-  {
-    name: "Ролл снежный краб",
-    description: "Снежный краб, кляр, сухари панко, рис, нори",
-    price: 350,
-    amount: "8 шт.",
-  },
-  {
-    name: "Сливочный ролл",
-    description: "Сливочный сыр, кляр, сухари панко, рис, нори",
-    price: 350,
-    amount: "8 шт.",
-  },
+    {
+        id: "husky",
+        name: "Хаски",
+        pieces: 8,
+        price: 450,
+        description: "Лосось, огурец, сыр творожный, кляр, сухари панко, рис, нори",
+    },
+    {
+        id: "salmon-roll-fried",
+        name: "Ролл с лососем",
+        pieces: 8,
+        price: 350,
+        description: "Лосось, кляр, сухари панко, рис, нори",
+    },
+    {
+        id: "chicken-roll-fried",
+        name: "Ролл с курицей",
+        pieces: 8,
+        price: 350,
+        description: "Курица, кляр, сухари панко, рис, нори",
+    },
+    {
+        id: "snow-crab-roll-fried",
+        name: "Ролл снежный краб",
+        pieces: 8,
+        price: 350,
+        description: "Снежный краб, кляр, сухари панко, рис, нори",
+    },
+    {
+        id: "cream-roll-fried",
+        name: "Сливочный ролл",
+        pieces: 8,
+        price: 350,
+        description: "Сливочный сыр, кляр, сухари панко, рис, нори",
+    },
 ];
