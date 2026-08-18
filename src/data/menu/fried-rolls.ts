5e8f868851b605b5583a315d6b46ae20018567ae
semperthY <hamzter76@gmail.com>
2026-08-18 15:58:17 +0300

    Add fried rolls menu section

---
+++ src/data/menu/fried-rolls.ts
@@ -0,0 +1,34 @@
+import type { MenuItem } from "./types";
+
+export const friedRolls: MenuItem[] = [
+  {
+    name: "Хаски",
+    description: "Лосось, огурец, сыр творожный, кляр, сухари панко, рис, нори",
+    price: 450,
+    amount: "8 шт.",
+  },
+  {
+    name: "Ролл с лососем",
+    description: "Лосось, кляр, сухари панко, рис, нори",
+    price: 350,
+    amount: "8 шт.",
+  },
+  {
+    name: "Ролл с курицей",
+    description: "Курица, кляр, сухари панко, рис, нори",
+    price: 350,
+    amount: "8 шт.",
+  },
+  {
+    name: "Ролл снежный краб",
+    description: "Снежный краб, кляр, сухари панко, рис, нори",
+    price: 350,
+    amount: "8 шт.",
+  },
+  {
+    name: "Сливочный ролл",
+    description: "Сливочный сыр, кляр, сухари панко, рис, нори",
+    price: 350,
+    amount: "8 шт.",
+  },
+];
