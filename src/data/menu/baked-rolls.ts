275b0793ced9621c3e566874893cf3758156c9cd
semperthY <hamzter76@gmail.com>
2026-08-18 16:00:37 +0300

    Add new baked rolls

--- src/data/menu/baked-rolls.ts
+++ src/data/menu/baked-rolls.ts
@@ -25,4 +25,47 @@ export const bakedRolls: MenuItem[] = [
         description:
             "Сливочный сыр, снежный краб, огурец, основа для запекания, креветка, соус терияки, кунжут, рис, нори",
     },
+    {
+        id: "imperator",
+        name: "Император",
+        pieces: 8,
+        price: 350,
+        description:
+            "Угорь, курица, томаты, основа для запекания, соус сладкий чили, ч/с кунжут, рис, нори",
+    },
+    {
+        id: "salmon-roll-baked",
+        name: "Ролл с лососем",
+        pieces: 8,
+        price: 350,
+        description: "Лосось, шапка для запекания, рис, нори",
+    },
+    {
+        id: "shrimp-roll-baked",
+        name: "Ролл с креветкой",
+        pieces: 8,
+        price: 350,
+        description: "Креветка, шапка для запекания, рис, нори",
+    },
+    {
+        id: "chicken-roll-baked",
+        name: "Ролл с курицей",
+        pieces: 8,
+        price: 350,
+        description: "Курица, шапка для запекания, рис, нори",
+    },
+    {
+        id: "snow-crab-roll-baked",
+        name: "Ролл снежный краб",
+        pieces: 8,
+        price: 350,
+        description: "Снежный краб, шапка для запекания, рис, нори",
+    },
+    {
+        id: "cream-roll-baked",
+        name: "Сливочный ролл",
+        pieces: 8,
+        price: 350,
+        description: "Сливочный сыр, шапка для запекания, рис, нори",
+    },
 ];
