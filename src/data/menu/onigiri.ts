9948b07c223cd0fd30da0b4131727fe24d3e4491
semperthY <hamzter76@gmail.com>
2026-08-18 15:58:31 +0300

    Add onigiri menu section

---
+++ src/data/menu/onigiri.ts
@@ -0,0 +1,22 @@
+import type { MenuItem } from "./types";
+
+export const onigiri: MenuItem[] = [
+  {
+    name: "Пикантная курица",
+    description: "Курица, японский майонез, салат айсберг, рис, нори",
+    price: 200,
+    amount: "",
+  },
+  {
+    name: "Дерзкая креветка",
+    description: "Креветка, соус спайси, огурец, икра масаго, рис, нори",
+    price: 250,
+    amount: "",
+  },
+  {
+    name: "Неугомонный лосось",
+    description: "Лосось, творожный сыр, огурец, рис, нори",
+    price: 300,
+    amount: "",
+  },
+];
