594ae83274155839a47b40e139116c617347e06f
semperthY <hamzter76@gmail.com>
2026-08-18 16:01:36 +0300

    Fix onigiri data shape

--- src/data/menu/onigiri.ts
+++ src/data/menu/onigiri.ts
@@ -1,22 +1,22 @@
 import type { MenuItem } from "./types";
 
 export const onigiri: MenuItem[] = [
-  {
-    name: "Пикантная курица",
-    description: "Курица, японский майонез, салат айсберг, рис, нори",
-    price: 200,
-    amount: "",
-  },
-  {
-    name: "Дерзкая креветка",
-    description: "Креветка, соус спайси, огурец, икра масаго, рис, нори",
-    price: 250,
-    amount: "",
-  },
-  {
-    name: "Неугомонный лосось",
-    description: "Лосось, творожный сыр, огурец, рис, нори",
-    price: 300,
-    amount: "",
-  },
+    {
+        id: "spicy-chicken-onigiri",
+        name: "Пикантная курица",
+        price: 200,
+        description: "Курица, японский майонез, салат айсберг, рис, нори",
+    },
+    {
+        id: "bold-shrimp-onigiri",
+        name: "Дерзкая креветка",
+        price: 250,
+        description: "Креветка, соус спайси, огурец, икра масаго, рис, нори",
+    },
+    {
+        id: "restless-salmon-onigiri",
+        name: "Неугомонный лосось",
+        price: 300,
+        description: "Лосось, творожный сыр, огурец, рис, нори",
+    },
 ];
